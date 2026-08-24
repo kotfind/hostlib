11c3c9c43111a91d6996bb25100ab330ae374c41
kotfind <kotfind@yandex.ru>
2026-08-24 20:36:04 +0300

    use hostlib in test flake

--- test/flake.nix
+++ test/flake.nix
@@ -5,11 +5,13 @@
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+    hostlib.url = "path:..";
   };
 
   outputs = {
     nixpkgs,
     home-manager,
+    hostlib,
     ...
   }: let
     inherit (nixpkgs.lib) nixosSystem;
@@ -20,11 +22,20 @@
       modules = [
         ./os.nix
 
+        hostlib.nixosModules.default
+
+        ./profile.nix
+
         homeManager
         (
           {...}: {
+            hostlib._currentHostName = "vm1";
+
             home-manager.users.test = {
-              imports = [./home.nix];
+              imports = [./home.nix ./profile.nix hostlib.homeManagerModules.default];
+
+              hostlib._currentHostName = "vm1";
+              hostlib._currentUserName = "test";
             };
 
             users.users.test = {
---
+++ test/profile.nix
@@ -0,0 +1,13 @@
+{
+  hostlib = {
+    hosts = {
+      vm1 = {};
+      vm2 = {};
+    };
+
+    users = {
+      root = {};
+      test = {};
+    };
+  };
+}
